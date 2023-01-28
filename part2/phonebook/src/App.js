5b4b68f82cf660b83e40d0a4620b5b2df5285a7b
Arif Wibawa <arifwb.work@gmail.com>
2023-01-28 15:21:45 +0700

    Exercises 2.16 - 2.17

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,7 @@
-import { useState, useEffect } from "react";
-import axios from 'axios'
+import { useState, useEffect, Fragment } from "react";
+import axios from "axios";
+import personServices from "./services/persons";
+import './index.css'
 
 const Filter = ({ search }) => (
   <div>
@@ -7,12 +9,19 @@ const Filter = ({ search }) => (
   </div>
 );
 
-const Persons = ({ persons }) =>
-  persons.map((x) => (
-    <p key={x.name}>
-      {x.name} {x.number}
-    </p>
-  ));
+const Persons = ({ persons, deleteButton }) =>
+  persons.map((x) => {
+    return (
+      <Fragment key={x.name}>
+        <p>
+          {x.name} {x.number}{" "}
+          <button onClick={() => deleteButton(x.name, x.id)}>delete</button>
+        </p>
+      </Fragment>
+    );
+  });
+
+const Notification = ({ messages, showElement, category }) => showElement ? <p className={category}>{messages}</p> : <></> 
 
 const PersonForm = (props) => {
   const { formHandler, newName, nameController, newNumber, numberController } =
@@ -39,38 +48,65 @@ const App = () => {
   const [filteredList, setFilteredList] = useState(persons);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [errorMessage, setErrorMessage] = useState('')
+  const [showElement,setShowElement] = useState(false)
+  const [notifColor, setNotifColor] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons')
-      .then(res => {
-        setPersons(res.data)
-        setFilteredList(res.data)
-      })
-  }, [])
-
+    axios.get("http://localhost:3001/persons").then((res) => {
+      setPersons(res.data)
+      setFilteredList(res.data);
+    });
+  }, []);
 
   const formHandler = (event) => {
     event.preventDefault();
-    for (let i of persons) {
+    for (let i of filteredList) {
       if (i.name === newName) {
-        alert(`${newName} is already added to phonebook`);
+        if (
+          window.confirm(
+            `${newName} is already added to phone book, replace the old number with a new one ?`
+          )
+        ) {
+          const sendUpdatednumber = {
+            name: i.name,
+            number: newNumber,
+            id: i.id,
+          };
+          personServices.updatePerson(i.id, sendUpdatednumber).then((res) => {
+            personServices.getAll().then((x) => {
+              setFilteredList([...x]);
+              setErrorMessage(`Updated ${newName} numbers`)
+              setNotifColor('success')
+              setShowElement(true)
+              setTimeout(() => {
+                setShowElement(false)
+              }, 3000)
+            });
+          });
+        }
         setNewName("");
         setNewNumber("");
         return;
       }
     }
 
-    const newInput = [
-      ...persons,
-      {
-        name: newName,
-        number: newNumber,
-      },
-    ];
+    const newInput = {
+      name: newName,
+      number: newNumber,
+    };
 
-    setPersons(newInput);
-    setFilteredList(newInput);
+    personServices.create(newInput).then((returnedData) => {
+      setPersons([...persons, returnedData]);
+      setFilteredList([...filteredList, returnedData]);
+    });
 
+    setErrorMessage(`Added ${newName}`)
+    setNotifColor('success')
+    setShowElement(true)
+    setTimeout(() => {
+      setShowElement(false)
+    }, 3000)
     setNewName("");
     setNewNumber("");
   };
@@ -99,6 +135,25 @@ const App = () => {
     setFilteredList(updatedList);
   };
 
+  const deletebutton = (person, id) => {
+    if (window.confirm(`Delete ${person} ?`)) {
+      
+      personServices.deletePerson(id).then((res) => {
+        personServices.getAll().then((res) => {
+          setFilteredList(res);
+          setPersons(res);
+        });
+      }).catch(err => {
+        setErrorMessage(`Information of ${person} has already been removed from server`)
+        setNotifColor('failed')
+        setShowElement(true)
+        setTimeout(() => {
+          setShowElement(false)
+        }, 3000)
+      })
+    }
+  };
+
   const eventHandler = {
     formHandler,
     newName,
@@ -110,11 +165,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification messages={errorMessage} showElement={showElement} category={notifColor}/>
       <Filter search={searchController} />
       <h3>add a new</h3>
       <PersonForm event={eventHandler} />
       <h3>Numbers</h3>
-      <Persons persons={filteredList} />
+      <Persons persons={filteredList} deleteButton={deletebutton} />
     </div>
   );
 };
